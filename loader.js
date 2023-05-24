e12a636bf1c5fe1d8c8b3316b783a9faa0c808d6
Василий Долгих <hazkorj@yandex.ru>
2023-05-24 21:42:31 +0500

    Initial commit

--- loader.js
+++ loader.js
@@ -2,12 +2,4 @@ const {createAudioPlayer} = require("@discordjs/voice");
 
 const player = createAudioPlayer();
 
-player.on('stateChange', subscription => {
-    subscription.onStreamError(() => {
-        setTimeout(() => {
-            player.play(subscription.resource);
-        }, 3000);
-    });
-});
-
 exports.player = player;
