8eb8fa42e184d32844fc4b66cdde6e9cb7679e82
Василий Долгих <hazkorj@yandex.ru>
2023-05-25 20:39:09 +0500

    Initial commit

--- commands/voice.js
+++ commands/voice.js
@@ -23,8 +23,6 @@ module.exports = {
             },
             opusEncoded: true,
             fec: true,
-            bitrate: 256000,
-            frameDuration: 60,
         });
         const info = await ytdl.getInfo(videoUrl);
 
@@ -34,7 +32,7 @@ module.exports = {
         });
         console.log(`Выбран формат: ${bestFormat.qualityLabel} (${bestFormat.audioBitrate}kbps аудио, ${bestFormat.bitrate}kbps видео)`);
 
-        const stream = ytdl(videoUrl,{quality: 'highestaudio', filter: 'audioonly'});
+        const stream = ytdl(videoUrl,{quality: 'highestaudio', filter: 'audioonly', format: bestFormat});
         player.on('error', err => {
             console.error('stream error' + err.message);
         });
